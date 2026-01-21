25818431ba34efc5d780df1f2366b7182c199069
agencianookweb-hash <agencia.nookweb@gmail.com>
2026-01-20 23:32:31 -0300

    fix: Alterar opÃ§Ãµes de paginaÃ§Ã£o para 5, 10 e 25 itens por pÃ¡gina

--- client/src/pages/Fornecedores.tsx
+++ client/src/pages/Fornecedores.tsx
@@ -209,8 +209,7 @@ export default function Fornecedores() {
                 >
                   <option value={5}>5</option>
                   <option value={10}>10</option>
-                  <option value={50}>50</option>
-                  <option value={100}>100</option>
+                  <option value={25}>25</option>
                 </select>
               </div>
 
